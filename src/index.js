b2fddd0d4dd91c994d92ac3153deb2e11aee5062
Luis Salcedo <luis.e.ss@gmail.com>
2023-04-15 10:51:36 -0600

    Complete TaskLister Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildToDO(e.target.querySelector("#new-task-description").value);
+  });
 });
+
+function buildToDO(todo) {
+  let p = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "X";
+  p.textContent = `${todo} `;
+  p.appendChild(btn);
+  let list = document.querySelector("#list");
+  list.querySelector("ul").appendChild(p);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
